0d2a7021037623ce952ac2dab21832749cfe66df
David Formánek <->
2013-04-26 21:29:55 +0200

    Abstract class to allow simple test cases for DOM modules.

---
+++ test/cz/muni/fi/mir/mathmlcanonicalization/modules/AbstractDOMModuleTest.java
@@ -0,0 +1,97 @@
+package cz.muni.fi.mir.mathmlcanonicalization.modules;
+
+import cz.muni.fi.mir.mathmlcanonicalization.Settings;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
+import org.junit.*;
+import org.xml.sax.SAXException;
+
+/**
+ * Abstract class to allow descendants to simple compare desired and produced xml
+ * 
+ * @author David Formanek
+ */
+@Ignore // no tests
+abstract class AbstractDOMModuleTest {
+
+    private static final String PATH_TO_TESTFILES = "/res/";
+    
+    protected void testXML(DOMModule instance, String testFile) throws IOException {
+        final InputStreamReader processed = getProcessed(instance, testFile);
+        final InputStreamReader canonical = getCanonical(testFile);
+        XMLUnit.setIgnoreWhitespace(true);
+        try {
+            new XMLTestCase(){}.assertXMLEqual(canonical, processed);
+        } catch (SAXException ex) {
+            Logger.getLogger(MfencedReplacerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    protected InputStreamReader getProcessed(DOMModule instance, String testFile) {
+        InputStream resource = this.getClass().
+                getResourceAsStream(PATH_TO_TESTFILES + testFile + ".original.xml");
+        return getProcessed(instance, resource);
+    }
+
+    protected InputStreamReader getCanonical(String testFile) {
+        InputStream resource = this.getClass().
+                getResourceAsStream(PATH_TO_TESTFILES + testFile + ".canonical.xml");
+        return new InputStreamReader(resource);
+    }
+    
+    /**
+     * Get input processed by specified module.
+     * 
+     * @param instance a configured instance of the tested module
+     * @param in input stream with original XML document
+     * @return stream reader for processed input
+     */
+    private InputStreamReader getProcessed(DOMModule instance, InputStream in) {
+        SAXBuilder builder = Settings.setupSAXBuilder();
+        Document doc;
+        try {
+            doc = builder.build(in);
+            instance.execute(doc);
+            
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            XMLOutputter serializer = new XMLOutputter();
+            serializer.output(doc, output);
+            
+            printDocument(output, "Output of " + instance);
+            
+            return new InputStreamReader(new ByteArrayInputStream(output.toByteArray()));
+            
+        } catch (JDOMException ex) {
+            Logger.getLogger(this.getClass().getName()).log(
+                    Level.SEVERE, "cannot build document", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(this.getClass().getName()).log(
+                    Level.SEVERE, "cannot convert between stream and DOM", ex);
+        }
+        return null;
+    }
+    
+    private void printDocument(ByteArrayOutputStream output, String header) {
+        System.out.println(header);
+        System.out.println("--------------------");
+        try {
+            output.writeTo(System.out);
+        } catch (IOException ex) {
+            Logger.getLogger(this.getClass().getName()).log(
+                    Level.WARNING, "cannot print result", ex);
+        }
+        System.out.println("--------------------");
+        System.out.println();
+    }
+}
--- test/cz/muni/fi/mir/mathmlcanonicalization/modules/MfencedReplacerTest.java
+++ test/cz/muni/fi/mir/mathmlcanonicalization/modules/MfencedReplacerTest.java
@@ -1,55 +1,20 @@
 package cz.muni.fi.mir.mathmlcanonicalization.modules;
 
-import cz.muni.fi.mir.mathmlcanonicalization.XMLTestUtils;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.custommonkey.xmlunit.XMLTestCase;
-import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 /**
  * Test for MfencedReplacer canonicalization DOM Module.
  * 
  * @author David Formanek
  */
-public class MfencedReplacerTest extends XMLTestCase {
+public class MfencedReplacerTest extends AbstractDOMModuleTest {
 
-    /**
-     * Test of interval transformation.
-     */
     @Test
     public void testSimpleInterval() throws IOException {
         MfencedReplacer instance = new MfencedReplacer();
-        System.out.println(instance);
-        testXML(instance, "interval");
+        testXML(instance, "mfencedreplacer/interval");
     }
 
     // TODO: add more tests
-    
-    private void testXML(DOMModule instance, String testFile) throws IOException {
-        final InputStreamReader processed = getProcessed(instance, testFile);
-        final InputStreamReader canonical = getCanonical(testFile);
-        XMLUnit.setIgnoreWhitespace(true);
-        try {
-            assertXMLEqual(canonical, processed);
-        } catch (SAXException ex) {
-            Logger.getLogger(MfencedReplacerTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private InputStreamReader getProcessed(DOMModule instance, String testFile) {
-        InputStream resource = this.getClass().
-                getResourceAsStream("/res/mfencedReplacer/" + testFile + ".original.xml");
-        return XMLTestUtils.getProcessed(instance, resource);
-    }
-
-    private InputStreamReader getCanonical(String testFile) {
-        InputStream resource = this.getClass().
-                getResourceAsStream("/res/mfencedReplacer/" + testFile + ".canonical.xml");
-        return new InputStreamReader(resource);
-    }
 }
