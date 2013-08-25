92bdc8148cbe12b703490e057fe833239c6f0c87
David Formánek <->
2013-08-25 22:53:20 +0200

    OperatorNormalizer refactoring (+minor changes), property names changed. Real &af; character in tests (issue - not working with entity).

--- src/cz/muni/fi/mir/mathmlcanonicalization/modules/OperatorNormalizer.java
+++ src/cz/muni/fi/mir/mathmlcanonicalization/modules/OperatorNormalizer.java
@@ -1,6 +1,8 @@
 package cz.muni.fi.mir.mathmlcanonicalization.modules;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -23,8 +25,15 @@ public class OperatorNormalizer extends AbstractModule implements DOMModule {
      */
     private static final String PROPERTIES_FILENAME = "/res/operator-normalizer.properties";
     
-    private List<String> removeList;
-    private List<String> functionOperatorsList;
+    // MathML elements
+    private static final String IDENTIFIER = "mi";
+    private static final String OPERATOR = "mo";
+    private static final String ROW = "mrow";
+    // properties key names
+    private static final String NORMALIZE_FUNCTIONS = "normalizefunctions";
+    private static final String APPLY_FUNCTION_OPERATORS = "functionoperators";
+    private static final String REMOVE_EMPTY_OPERATORS = "removeempty";
+    private static final String OPERATORS_TO_REMOVE = "removeoperators";
     
     public OperatorNormalizer() {
         loadProperties(PROPERTIES_FILENAME);
@@ -32,109 +41,128 @@ public class OperatorNormalizer extends AbstractModule implements DOMModule {
     
     @Override
     public void execute(final Document doc) {
-        if (isEnabled("remove_empty") || isEnabled("remove")) {
-            removeList = Arrays.asList(getProperty("remove.operators").split(" "));
-            removeSpareOperators(doc.getRootElement());
+        if (doc == null) {
+            throw new IllegalArgumentException("document is null");
         }
-        if (isEnabled("function_app")) {
-            functionOperatorsList = Arrays.asList(getProperty("function_app.operators").split(" "));
-            normalizeFunctionApplication(doc.getRootElement());
+        final Element root = doc.getRootElement();
+        if (isEnabled(REMOVE_EMPTY_OPERATORS) || !getProperty(OPERATORS_TO_REMOVE).isEmpty()) {
+            removeSpareOperators(root, getPropertyCollection(OPERATORS_TO_REMOVE));
+        }
+        if (isEnabled(NORMALIZE_FUNCTIONS)) {
+            normalizeFunctionApplication(root, getPropertyCollection(APPLY_FUNCTION_OPERATORS));
         }
     }
     
-    private void removeSpareOperators(final Element element) {
+    private void removeSpareOperators(final Element element, Collection<String> spareOperators) {
         final List<Element> children = element.getChildren();
         for (int i = 0; i < children.size(); i++) {
             final Element actual = children.get(i); // actual element
-            
-            if (actual.getName().equals("mo")) {
-                if ((isEnabled("remove_empty") && actual.getText().isEmpty())
-                || (isEnabled("remove") && removeList.contains(actual.getText()))) {
+            if (isOperator(actual)) {
+                if (isSpareOperator(actual, spareOperators)){
                     actual.detach();
                     i--; // move iterator back after detaching so it points to next element
                 }
             } else {
-                removeSpareOperators(actual);
+                removeSpareOperators(actual, spareOperators);
             }
         }
     }
     
-    private void normalizeFunctionApplication(final Element element) {
-        final List<Element> children = element.getChildren(); // this list is "live" changes to it changes element
+    private boolean isSpareOperator(final Element operator, final Collection<String> spareOperetors) {
+        return (isEnabled(REMOVE_EMPTY_OPERATORS) && operator.getText().isEmpty())
+                || (spareOperetors.contains(operator.getText()));
+    }
+    
+    private void normalizeFunctionApplication(final Element element,
+            final Collection<String> functionOperators) {
+        final List<Element> children = element.getChildren();
         for (int i = 0; i < children.size(); i++) {
-            if ((i < children.size()-2)
-                    && children.get(i).getName().equals("mi")
-                    && children.get(i+1).getName().equals("mo")
-                    && functionOperatorsList.contains(children.get(i+1).getText())) { // check for function app operators
-                final int parPos = i + 2;
-                final Element parameter = children.get(parPos);
+            if (isFunction(i, children, functionOperators)) {
+                final int parameterPosition = i + 2;
+                Element parameter = children.get(parameterPosition);
+                // mrow in which the parameter will be stored
+                final Element newParameter = new Element(ROW);
                 
-                final Element newParameter = new Element("mrow"); // mrow in which the parameter will be stored
-                
-                if (parameter.getName().equals("mrow")) {
+                if (parameter.getName().equals(ROW)) {
                     if(hasInsideBrackets(parameter)) {
-                        children.get(i + 1).detach(); // mrow is in right format -> just detach operator
-                    } else {
-                        parameter.addContent(1, new Element("mo").setText("(")); // add left bracket to mrow
-                        parameter.addContent(new Element("mo").setText(")")); // add right bracket to mrow
+                        children.get(i + 1).detach(); // just detach operator
+                    } else { // add parentheses
+                        parameter.addContent(1, new Element(OPERATOR).setText("("));
+                        parameter.addContent(new Element(OPERATOR).setText(")"));
                         children.get(i + 1).detach(); // detach funct app operator
                     }
                     continue; // no need to set newParameter
-                } else if (parameter.getName().equals("mo")
-                               && parameter.getText().equals("(")) {
+                } else if (isOperator(parameter, "(")) {
                     int bracketsDepth = 1;
-                    newParameter.addContent(children.get(parPos).detach());
+                    newParameter.addContent(parameter.detach());
                     
-                    while ((parPos < children.size()) && (bracketsDepth > 0)) {
-                        if (children.get(parPos).getName().equals("mo")
-                                && children.get(parPos).getText().equals("(")) {
+                    while ((parameterPosition < children.size()) && (bracketsDepth > 0)) {
+                        parameter = children.get(parameterPosition);
+                        if (isOperator(parameter, "(")) {
                             bracketsDepth++;
                         }
-                        else if (children.get(parPos).getName().equals("mo")
-                                && children.get(parPos).getText().equals(")")) {
+                        else if (isOperator(parameter, ")")) {
                             bracketsDepth--;
                         }
-                        newParameter.addContent(children.get(parPos).detach());
+                        newParameter.addContent(parameter.detach());
                     }
                     for (; bracketsDepth > 0; bracketsDepth--) { // add missing right brackets
-                        newParameter.addContent(new Element("mo").setText(")"));
+                        newParameter.addContent(new Element(OPERATOR).setText(")"));
                     }
                 } else { // if the paramether is neither mrow or (
-                    newParameter.addContent(new Element("mo").setText("(")); // add left bracket
-                    newParameter.addContent(children.get(parPos).detach());
-                    newParameter.addContent(new Element("mo").setText(")")); // add right bracket
+                    newParameter.addContent(new Element(OPERATOR).setText("(")); // add left bracket
+                    newParameter.addContent(children.get(parameterPosition).detach());
+                    newParameter.addContent(new Element(OPERATOR).setText(")")); // add right bracket
                 }
-                
                 children.set(i + 1, newParameter); // replace function app operator with newParameter
             } else { // if there isnt start of function application apply normalization on children
-                normalizeFunctionApplication(children.get(i));
+                normalizeFunctionApplication(children.get(i), functionOperators);
             }
-            
         }
+    }
+    
+    private boolean isFunction(int i, final List<Element> children,
+            final Collection<String> functionOperators) {
+        return ((i < children.size() - 2)
+                && children.get(i).getName().equals(IDENTIFIER)
+                && isOperator(children.get(i+1))
+                && functionOperators.contains(children.get(i+1).getText()));
     }
     
     private boolean hasInsideBrackets(final Element mrow) {
         final List<Element> children = mrow.getChildren();
-        if ((children.size() > 1) && children.get(0).getName().equals("mo")
-                && children.get(0).getText().equals("(")) {
+        if ((children.size() > 1) && isOperator(children.get(0), "(")) {
             int bracketsDepth = 1;
+            Element child;
             for (int i = 1; i < children.size(); i++) {
-                if (children.get(i).getName().equals("mo") && children.get(i).getText().equals("(")) {
+                child = children.get(i);
+                if (isOperator(child, "(")) {
                     bracketsDepth++;
-                }
-                else if (children.get(i).getName().equals("mo") && children.get(i).getText().equals(")")) {
+                } else if (isOperator(child, ")")) {
                     bracketsDepth--;
                 }
                 if (bracketsDepth == 0) {
-                    if (i < children.size()-1) {
+                    if (i < children.size() - 1) {
                         return false;
                     }
-                    if (i == children.size()-1) {
+                    if (i == children.size() - 1) {
                         return true;
                     }
                 }
             }
         }
         return false;
+    }
+    
+    private boolean isOperator(final Element element, final String operator) {
+        return isOperator(element) && element.getText().equals(operator);
+    }
+    
+    private boolean isOperator(final Element element) {
+        return element.getName().equals(OPERATOR);
+    }
+    
+    private Collection<String> getPropertyCollection(final String property) {
+        return new HashSet<String>(Arrays.asList(getProperty(property).split(" ")));
     }
 }
