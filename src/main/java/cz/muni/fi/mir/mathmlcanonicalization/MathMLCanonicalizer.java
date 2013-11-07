9f3605ed4cbc9e1ad45675b55a89f6356a694f67
Michal Růžička <ruzicka.mich@gmail.com>
2013-11-08 00:41:13 +0100

    Input preprocessing  – always add XHTML+MathML 1.1 Doctype to allow MathML named entities processing.

--- src/main/java/cz/muni/fi/mir/mathmlcanonicalization/MathMLCanonicalizer.java
+++ src/main/java/cz/muni/fi/mir/mathmlcanonicalization/MathMLCanonicalizer.java
@@ -15,7 +15,10 @@
  */
 package cz.muni.fi.mir.mathmlcanonicalization;
 
+import gnu.regexp.RE;
 import cz.muni.fi.mir.mathmlcanonicalization.modules.*;
+import gnu.regexp.REException;
+import gnu.regexp.REFilterInputStream;
 import java.io.*;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -281,15 +284,24 @@ public final class MathMLCanonicalizer {
      * @throws ModuleException some module cannot canonicalize the input
      */
     public void canonicalize(final InputStream in, final OutputStream out)
-            throws JDOMException, IOException, ModuleException {
+            throws JDOMException, IOException, ModuleException, REException {
         if (in == null) {
             throw new NullPointerException("in");
         }
         if (out == null) {
             throw new NullPointerException("out");
         }
-        InputStream inputStream = in;
+
+        InputStream inputStream = new REFilterInputStream(
+                new REFilterInputStream(in, new RE("<!DOCTYPE [^>]+>\n?"), ""),
+                new RE("(<\\?xml.*\\?>)"),
+                "$1\n<!DOCTYPE math SYSTEM \"xhtml-math11.dtd\">");
         ByteArrayOutputStream outputStream = null;
+
+//        StringWriter writer = new StringWriter();
+//        IOUtils.copy(inputStream, writer);
+//        String theString = writer.toString();
+//        System.out.println(theString);
 
         // calling stream modules
         for (StreamModule module : streamModules) {
